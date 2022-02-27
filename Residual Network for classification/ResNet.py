342414709e113581466cd0969db48b5e2e464bce
Furkan can <84248758+FurkanCan-eee@users.noreply.github.com>
2022-02-27 20:19:06 +0300

    Update ResNet.py

--- Residual Network for classification/ResNet.py
+++ Residual Network for classification/ResNet.py
@@ -13,7 +13,7 @@ from helper_codes import *
 from tensorflow.keras.initializers import random_uniform, glorot_uniform, constant, identity
 from tensorflow.python.framework.ops import EagerTensor
 from matplotlib.pyplot import imshow
-
+from tensorflow.keras.datasets import mnist
 
 # Identity block : The identity block is the standard block used in ResNets, and corresponds to the 
 # case where the input activation (say a^[l] ) has the same dimension as the output activation (say a^[l+2]).
@@ -60,30 +60,6 @@ def identity_block(X, f, filters, training=True, initializer=random_uniform):
 
     return X
   
-""" One can run these codes to check the function above
-
-X1 = np.ones((1, 4, 4, 3)) * -1
-X2 = np.ones((1, 4, 4, 3)) * 1
-X3 = np.ones((1, 4, 4, 3)) * 3
-
-X = np.concatenate((X1, X2, X3), axis = 0).astype(np.float32)
-
-A3 = identity_block(X, f=2, filters=[4, 4, 3],
-                   initializer=lambda seed=0:constant(value=1),
-                   training=False)
-print('\033[1mWith training=False\033[0m\n')
-A3np = A3.numpy()
-print(np.around(A3.numpy()[:,(0,-1),:,:].mean(axis = 3), 5))
-resume = A3np[:,(0,-1),:,:].mean(axis = 3)
-print(resume[1, 1, 0])
-
-print('\n\033[1mWith training=True\033[0m\n')
-np.random.seed(1)
-A4 = identity_block(X, f=2, filters=[3, 3, 3],
-                   initializer=lambda seed=0:constant(value=1),
-                   training=True)
-print(np.around(A4.numpy()[:,(0,-1),:,:].mean(axis = 3), 5))
-"""
 
 # Convolutional block : The ResNet "convolutional block" is the second block type. You can use this 
 # type of block when the input and output dimensions don't match up. The difference with the identity 
@@ -142,7 +118,7 @@ def convolutional_block(X, f, filters, s = 2, training=True, initializer=glorot_
   
 # ResNet-50 model (Residual Network with 50 layers)
 
-def ResNet50(input_shape = (64, 64, 3), classes = 6):
+def ResNet50(input_shape = (64, 64, 3), classes = 10):
     """
     Stage-wise implementation of the architecture of the popular ResNet50:
     CONV2D -> BATCHNORM -> RELU -> MAXPOOL -> CONVBLOCK -> IDBLOCK*2 -> CONVBLOCK -> IDBLOCK*3
@@ -194,7 +170,7 @@ def ResNet50(input_shape = (64, 64, 3), classes = 6):
     X = identity_block(X, 3, [512, 512, 2048])
 
     ## AVGPOOL
-    X = AveragePooling2D((2, 2))(X)
+    X = AveragePooling2D((1, 1))(X)
     
     # output layer
     X = Flatten()(X)
@@ -205,9 +181,11 @@ def ResNet50(input_shape = (64, 64, 3), classes = 6):
 
     return model
   
+
+model = ResNet50(input_shape = (28, 28, 1), classes = 10)
+
 """ One can check the model summary whether the model runs correctly.
 
-model = ResNet50(input_shape = (64, 64, 3), classes = 6)
 print(model.summary())
 """
 
@@ -217,17 +195,17 @@ model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accur
 
 # Uploading the SIGNS dataset
 
-X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+(X_train_orig, Y_train_orig), (X_test_orig, Y_test_orig) = mnist.load_data()
 
 # Normalize image vectors
 
-X_train = X_train_orig / 255.
-X_test = X_test_orig / 255.
+X_train = (X_train_orig / 255.).reshape((X_train_orig.shape[0], 28, 28, 1))
+X_test = (X_test_orig / 255.).reshape((X_test_orig.shape[0], 28, 28, 1))
 
 # Convert training and test labels to one hot matrices
 
-Y_train = convert_to_one_hot(Y_train_orig, 6).T
-Y_test = convert_to_one_hot(Y_test_orig, 6).T
+Y_train = convert_to_one_hot(Y_train_orig, 10).T
+Y_test = convert_to_one_hot(Y_test_orig, 10).T
 
 # Shape of the dataset
 print ("number of training examples = " + str(X_train.shape[0]))
@@ -246,26 +224,3 @@ model.fit(X_train, Y_train, epochs = 10, batch_size = 32)
 preds = model.evaluate(X_test, Y_test)
 print ("Loss = " + str(preds[0]))
 print ("Test Accuracy = " + str(preds[1]))
-
-# Pre-trained model and its weights using GPU (saved in tenserflow)
-
-pre_trained_model = tf.keras.models.load_model('resnet50.h5')
-
-preds = pre_trained_model.evaluate(X_test, Y_test)
-print ("Loss = " + str(preds[0]))
-print ("Test Accuracy = " + str(preds[1]))
-
-"""
-# Testing the model with our own hand images 
-
-img_path = 'name_of_the_image_file/image_name.jpg'
-img = image.load_img(img_path, target_size=(64, 64))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = x/255.0
-print('Input image shape:', x.shape)
-imshow(img)
-prediction = pre_trained_model.predict(x)
-print("Class prediction vector [p(0), p(1), p(2), p(3), p(4), p(5)] = ", prediction)
-print("Class:", np.argmax(prediction))
-"""
