4024e327ef5a5ba7519ae95868498bfd2c173f18
Furkan can <84248758+FurkanCan-eee@users.noreply.github.com>
2022-03-11 19:12:21 +0300

    Create Callback_function.py

---
+++ Callback/Callback_function.py
@@ -0,0 +1,115 @@
+İmport tensorflow as tf
+
+"""
+An overview of callback methods
+Global methods
+on_(train|test|predict)_begin(self, logs=None)
+Called at the beginning of fit/evaluate/predict.
+
+on_(train|test|predict)_end(self, logs=None)
+Called at the end of fit/evaluate/predict.
+
+Batch-level methods for training/testing/predicting
+on_(train|test|predict)_batch_begin(self, batch, logs=None)
+Called right before processing a batch during training/testing/predicting.
+
+on_(train|test|predict)_batch_end(self, batch, logs=None)
+Called at the end of training/testing/predicting a batch. Within this method, logs is a dict containing the metrics results.
+
+Epoch-level methods (training only)
+on_epoch_begin(self, epoch, logs=None)
+Called at the beginning of an epoch during training.
+
+on_epoch_end(self, epoch, logs=None)
+Called at the end of an epoch during training.
+"""
+
+# Define the Keras model to add callbacks to
+def get_model():
+    model = keras.Sequential()
+    model.add(keras.layers.Dense(1, input_dim=784))
+    model.compile(
+        optimizer=keras.optimizers.RMSprop(learning_rate=0.1),
+        loss="mean_squared_error",
+        metrics=["mean_absolute_error"],
+    )
+    return model
+
+# Load example MNIST data and pre-process it
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train = x_train.reshape(-1, 784).astype("float32") / 255.0
+x_test = x_test.reshape(-1, 784).astype("float32") / 255.0
+
+class CustomCallback(keras.callbacks.Callback):
+    def on_train_begin(self, logs=None):
+        keys = list(logs.keys())
+        print("Starting training; got log keys: {}".format(keys))
+
+    def on_train_end(self, logs=None):
+        keys = list(logs.keys())
+        print("Stop training; got log keys: {}".format(keys))
+
+    def on_epoch_begin(self, epoch, logs=None):
+        keys = list(logs.keys())
+        print("Start epoch {} of training; got log keys: {}".format(epoch, keys))
+
+    def on_epoch_end(self, epoch, logs=None):
+        keys = list(logs.keys())
+        print("End epoch {} of training; got log keys: {}".format(epoch, keys))
+
+    def on_test_begin(self, logs=None):
+        keys = list(logs.keys())
+        print("Start testing; got log keys: {}".format(keys))
+
+    def on_test_end(self, logs=None):
+        keys = list(logs.keys())
+        print("Stop testing; got log keys: {}".format(keys))
+
+    def on_predict_begin(self, logs=None):
+        keys = list(logs.keys())
+        print("Start predicting; got log keys: {}".format(keys))
+
+    def on_predict_end(self, logs=None):
+        keys = list(logs.keys())
+        print("Stop predicting; got log keys: {}".format(keys))
+
+    def on_train_batch_begin(self, batch, logs=None):
+        keys = list(logs.keys())
+        print("...Training: start of batch {}; got log keys: {}".format(batch, keys))
+
+    def on_train_batch_end(self, batch, logs=None):
+        keys = list(logs.keys())
+        print("...Training: end of batch {}; got log keys: {}".format(batch, keys))
+
+    def on_test_batch_begin(self, batch, logs=None):
+        keys = list(logs.keys())
+        print("...Evaluating: start of batch {}; got log keys: {}".format(batch, keys))
+
+    def on_test_batch_end(self, batch, logs=None):
+        keys = list(logs.keys())
+        print("...Evaluating: end of batch {}; got log keys: {}".format(batch, keys))
+
+    def on_predict_batch_begin(self, batch, logs=None):
+        keys = list(logs.keys())
+        print("...Predicting: start of batch {}; got log keys: {}".format(batch, keys))
+
+    def on_predict_batch_end(self, batch, logs=None):
+        keys = list(logs.keys())
+        print("...Predicting: end of batch {}; got log keys: {}".format(batch, keys))
+        
+model = get_model()
+model.fit(
+    x_train,
+    y_train,
+    batch_size=128,
+    epochs=1,
+    verbose=0,
+    validation_split=0.5,
+    callbacks=[CustomCallback()],
+)
+
+res = model.evaluate(
+    x_test, y_test, batch_size=128, verbose=0, callbacks=[CustomCallback()]
+)
+
+res = model.predict(x_test, batch_size=128, callbacks=[CustomCallback()])
